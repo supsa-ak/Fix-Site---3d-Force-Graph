1b1a99ee9b4f16147b2e62ca7811fa036c3b8498
Sarthak <sarthakkalpande113@gmail.com>
2021-08-24 01:10:05 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, escape, request, render_template, jsonify, send_from_directory, url_for, redirect
+import urllib.request
+import json 
+
+
+app = Flask(__name__)
+
+def write_json(data, filename="datasets/nodes.json"):
+	with open(filename, "w") as f:
+		json.dump(data, f, indent=4)
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+	print("_______INDEX___________")
+	f = open('datasets/nodes.json')
+	data = json.load(f)
+	if request.method == "POST":
+		node_id = request.form["nodeid"]
+		node_desc = request.form["nodedesc"]
+		node_grp = request.form["nodegrp"]
+		node_conn = request.form["nodeconn"]
+		# print(node_id)
+		
+		with open ("datasets/nodes.json") as json_file:
+			data = json.load(json_file)
+			temp = data["nodes"]
+			no = len(temp)
+			# print('this is no', no)
+			y = {
+					"id": node_id,
+					"group": node_grp,
+					"description": node_desc, 
+					"val": 20, 
+					"links":[
+						{"target": node_conn}
+					]
+				}
+			temp.append(y)
+
+			temp2  = data["links"]
+			no2 = len(temp2)
+			k = {
+					"source": node_id,
+					"target": node_conn,
+					"value": 1
+				}
+			temp2.append(k)
+
+		write_json(data)
+	# print(data)
+	return render_template('index.html', data=data)
+
+
+@app.route('/datasets/<path:path>')
+def send_js(path):
+    return send_from_directory('datasets', path)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
---
+++ static/core.js
@@ -0,0 +1,8 @@
+class NodeCore {
+    constructor() {
+        this.window = window;
+    };
+    onNodeClick = async (node)=>{
+        console.log("node was clicked: ",node);
+    };
+}
