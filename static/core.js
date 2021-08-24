498a153e49a5a1a944474d106e6538aa68b0d23c
Sarthak <sarthakkalpande113@gmail.com>
2021-08-24 17:00:39 +0530

    added heading new node

--- static/core.js
+++ static/core.js
@@ -6,10 +6,12 @@ class NodeCore {
         // console.log("node was clicked: ",node);
         // console.log(node['id']);
 
+        document.getElementById("heading_current_node").style.display = "initial";
         document.getElementById("create_new_node_btn").style.display = "initial";
         document.getElementById("update_node").style.display = "initial";
         if(document.getElementById("submit-create-new-node")){
             document.getElementById("submit-create-new-node").remove();
+            document.getElementById("heading_new_node").remove();
         }
         document.getElementById('nodeid').value = node['id']; 
         document.getElementById('nodedesc').value = node['description']; 
@@ -29,6 +31,7 @@ function create_new_node(){
     document.getElementById('nodegrp').value = ""; 
     document.getElementById('nodeconn').value = "";
 
+    document.getElementById("heading_current_node").style.display = "none";
     document.getElementById("create_new_node_btn").style.display = "none";
     document.getElementById("update_node").style.display = "none";
     var tag = document.createElement("button");
@@ -39,10 +42,10 @@ function create_new_node(){
     tag.appendChild(text);
     var element = document.getElementById("create-btn");
     element.appendChild(tag);
-}
-
-function refreshForm(){
-    var container = document.getElementById("rightPanelMenu");
-    var content = container.innerHTML;
-    container.innerHTML = content;
+   
+    var tag = document.createElement("h3");
+    var text = document.createTextNode("New Node");
+    tag.appendChild(text);
+    var element = document.getElementById("heading_new_node");
+    element.appendChild(tag);
 }
